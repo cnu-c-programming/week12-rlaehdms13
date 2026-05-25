872a2927759888d06e9463c9c982146eb7d4e8dc
rlaehdms13 <de060103@gmail.com>
2026-05-25 17:59:50 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -6,6 +6,17 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(argv[1], "r");
 
+    int counts[26] = {0};
+    int c;
+
+    while ((c = fgetc(fp)) != EOF) {
+        if (c >= 'a' && c <= 'z') {
+            counts[c - 'a']++;
+        }
+    }
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'a' + i, counts[i]);
+    }
 
 
     fclose(fp);
