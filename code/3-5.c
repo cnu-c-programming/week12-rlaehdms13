a4ebb1d987500beeeee33de7c9b8b4918eb341ff
rlaehdms13 <de060103@gmail.com>
2026-05-26 13:52:25 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -7,10 +7,21 @@ int main(int argc, const char* argv[]) {
     if (fp == NULL) {
         return 0;
     }
+int sum = 0;
+    int num;
+    char invalid_token[64];
 
+    while (!feof(fp)) {
+        int result = fscanf(fp, "%d", &num);
 
-    int sum = 0;
-
+        if (result == 1) {
+            sum += num;
+        } 
+        else if (result == 0) {
+            fscanf(fp, "%s", invalid_token);
+            fprintf(stderr, "invalid input %s\n", invalid_token);
+        }
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
