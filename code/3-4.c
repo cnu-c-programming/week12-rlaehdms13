3801738353613353380e53650e900435d0b2dda7
rlaehdms13 <de060103@gmail.com>
2026-05-26 13:50:50 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
+    if(argc < 3){
+        return 0;
+    }
+    FILE* fp_src = fopen(argv[1], "r");
+    if (fp_src == NULL) {
+        perror("error: cannot open\n");
+        return 1;
+    }
+   FILE* fp_dst = fopen(argv[2], "w");
+    if (fp_dst == NULL) {
+        perror("error: cannot open\n");
+        fclose(fp_src);
+        return 1;
+    }
+    int c;
+    while ((c = fgetc(fp_src)) != EOF) {
+        if (fputc(c, fp_dst) == EOF) {
+            perror("error: cannot write\n"");
+            fclose(fp_src);
+            fclose(fp_dst);
+            return 1;
+        }
+    }
+    if (ferror(fp_src)) {
+        perror("error: cannot read\n");
+    }
 
-    "error: cannot open\n"
-    "error: cannot read\n"
-    "error: cannot write\n"
-
+    fclose(fp_src);
+    fclose(fp_dst);
+    return 0;
 }
+    
+
 
